722f49de6f00b74da56b488bbf53ebccae1bc4db
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-08-04 18:07:53 +0000

    updating print and string stuff

--- week_3/print_func_strings_triple_quote_strings_sec2.4_2.5.py
+++ week_3/print_func_strings_triple_quote_strings_sec2.4_2.5.py
@@ -1,53 +1,24 @@
 """
-print function and strings
+Print function and strings
 """
 
-# comma separated list
+# comma separated items
 age = 98
 print("age: ", age)
 
 # escape characters
-print("Welcome\nto\n\tPython!")
+print("welcome \nto \n\tpython")
 
-print("Backslash: \\")
-
-# line break
-print("This print statement is really really really long \
-and I want to put it onto 2 lines")
-
-
-# printing an expression
-print("sum of 3 and 5: ", 3+5)
-
-first_name = "Chelsea"
-last_name = 'Harding'
-
-print("my name is: ", first_name, last_name)
-print("my name is: " + first_name + last_name)
+# print the result of an expression
+print("Sum of 5 and 3:", 3+5)
 
 """
 Triple Quote Strings
 """
 
-# including quotes in strings
-# you can include double quotes inside single quote strings
-print('Display "hi" in quotes')
-print("Display \"hi\" in quotes")
-print("Display 'hi' in quotes")
+quote = "It's nice sunny today!"
 
-# triple line string
-story = """Hi this is my string.
-This is a really long string, so I am using triple double quotes
-to store everything in here.
+print("""My instrutor said "It's nice and sunny today." which made me mad.""")
 
-To continue my story, I wanted to put another line here!
-
-"""
-print("story: ", story)
-
-# triple single quote comments
-'''
-This is a comment, similar to # hashtag character but this is 
-used for multi-line comments. Usually at the top of your code to 
-provide documentation or an explaination of your code
-'''
+# double or single 
+str1 = 'Chelsea'
