f07a3d5431dbc1d776deaef767d8871f007c0104
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-08-06 19:40:19 +0000

    nested statement

--- week_4/nested_statements_sec_3.12.py
+++ week_4/nested_statements_sec_3.12.py
@@ -1,12 +1,8 @@
 """
-Nested Statements
+Nested Statement
 """
 
-# nested statements referes to statements inside of statements
-# sentinel_values has if statements inside of while loops
-
-# nested for loops
-for row in range(3):
-    for col in range (4):
-        print(row * col, " ", end="")
+for i in range(3):
+    for j in range(4):
+        print(i * j, " ", end="")
     print()
