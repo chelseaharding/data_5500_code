9d5d4271cea8aee0ed50bc826edfca9c9489146f
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-08-06 18:54:08 +0000

    sentinels

--- week_4/sentinel_values_sec_3.11.py
+++ week_4/sentinel_values_sec_3.11.py
@@ -2,18 +2,20 @@
 Sentinel Values
 """
 
-# program that executes a while loop with a sentinel
 selection = 0
+
 while selection != 5:
     print("Welcome to the happy message program!")
-    print("Enter one of the following selections...")
+    print("Enter one of the following selections:")
     print("1. Show the weather")
-    print("2. How's my day going to go?")
+    print("2. How is my day going to go?")
+    print("Select 5 to quit")
+
     selection = int(input("Enter your selection: "))
-    if selection == 1:
-        print("The weather is beautiful!\n")
-    if selection == 2:
-        print("Your day will be amazing!\n")
-    if selection == 5:
-        print("Thanks for coming!\n")
     
+    if selection == 1:
+        print("The weather is great!")
+    elif selection == 2:
+        print("Your day will be amazing!")
+    elif selection == 5:
+        print("Thanks for using the happy message program!")
