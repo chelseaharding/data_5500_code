2873b9e0c013853a86c1febc5fd158124ffb45d8
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-08-10 19:10:17 +0000

    updates for week 4

--- week_4/list_append_sec_5.10.py
+++ week_4/list_append_sec_5.10.py
@@ -1,21 +1,40 @@
 """
-List Append
+list appending
 """
 
-# create a list of even numbers, using the append function
 lst = []
 for i in range(10):
-    lst.append(i * 2)
-print("lst: ", lst)
+    lst.append(i)
 
+print("lst:", lst)
 
+lst.append(10)
+print("lst:", lst)
 
-# checkpoint activity
-# create a list of 10 random numbers using a for loop, randint, and append functions
-# meaning start with an empty list, then loop 10 times, adding a random number each time
-import random
-lst = []
-for i in range(10):
-    lst.append(random.randint(1,10))
-    
-print("lst: ", lst)
+# insert
+
+lst.insert(0, -1)
+print("lst:", lst)
+
+lst.insert(5, 3.5)
+print("lst:", lst)
+
+# extend kind of like augmented assignment (+=)
+lst.extend((11, 12, 13, 14))
+print("lst:", lst)
+
+# remove
+lst.remove(10)
+print("lst:", lst)
+
+# pop
+lst.pop()
+print("lst:", lst)
+
+# clear
+# lst.clear()
+# print("lst:", lst)
+
+# count
+for num in lst:
+    print(num, "is found", lst.count(num), "times")
--- week_4/list_slicing_sec_5.5.py
+++ week_4/list_slicing_sec_5.5.py
@@ -1,74 +1,17 @@
 """
-List Slicing
+List slicing
 """
 
-# slicing operator
-scores = [45, 39, 50, 49, 49, 44]
+scores = [45, 39, 56, 23, 44, 100, 99]
+scores_slice = scores[0:5]
+print("slice:", scores_slice)
 
-for i in range(1,5):
-    print(scores[i])
-    
-scores_sliced = scores[1:5]
+# reverse a list
 
-print(scores_sliced)
+scores_reversed = scores[::-1]
+print("reversed socres:", scores_reversed)
 
-    
-    
-input("press enter")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# slice from beginning
-print(scores[:4])
-
-# slice to end
-print(scores[2:])
-
-# checkpoint activity
-# create a list storing the ages of the individuals
-# in your family (feel free to make them up)
-# use the slice operator to pull out the first 3 values in the list
-# use the slice operator to pull out the last 3 values in the list
-# use the slice operator to pull out the second and third values
-# in the list
-ages = [39, 38, 14, 12, 10, 2]
-
-print(ages[:3]) 
-print(ages[3:])
-print(ages[1:3])
-
-
-name = "Professor H."
-print("name: ", name)
-print("name[::-1]: ", name[::-1])
-
-name = "bob"
-print(name == name[::-1])
-
-
-def palindrome(strng):
-    return strng == strng[::-1]
-    
-print(palindrome("racecar"))
-print(palindrome("racecar2"))
-print(palindrome("gohangasalamiimalasagnahog"))
-
-print(palindrome("amanaplanacanalpanama"))
-print(palindrome("ratsliveonnoevilstar"))
+# string
+hello = "Hello World!"
+hello_reversed = hello[::-1]
+print(hello_reversed)
