407d43e835ec55402a352c352916b328516b400b
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2026-08-17 17:47:20 +0000

    last update for week 5

--- week_5/string_split_sec_8.9.py
+++ week_5/string_split_sec_8.9.py
@@ -1,62 +1,26 @@
 """
-String Split and Join
+Splitting and Joining Strings
 """
 
 # split
-data_str = "1,2,3,4,5,6"
-lst = data_str.split(",")
-print("data_str: ", data_str)
-print("lst: ", lst)
+data_str = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+nums = data_str.split(" ")
+print("nums:", nums)
 
 # join
-new_str = ",".join(lst)
-print("new_str: ", new_str)
+new_str = " ".join(nums)
+print("new str:", new_str)
 
-
-new_str = ";".join(lst)
-print("new_str: ", new_str)
+new_str = ";".join(nums)
+print("new str:", new_str)
 
 # join with list comprehension
-lst = []
-for i in range(1,101):
-    lst.append(str(i))
-    
-print(lst)
+# lst = []
+# for i in range(100):
+#     lst.append(i)
 
-print(",".join(lst))
+# print(lst)
 
-str_lst = ",".join([str(i) for i in range(1,101) ])
+str_lst = ", ".join([str(i) for i in range(100)])
+print(str_lst)
 
-print("str_lst: ", str_lst)
-
-
-
-
-
-# checkpoint activity
-# Start with a string "1 2 3 4 5 6 7 8 9 10"
-# split the string into a list
-# Using a list comprehension convert the list 
-# from a list of strings to a list of ints
-
-
-
-
-
-
-
-
-
-
-
-
-
-# solution
-string = "1 2 3 4 5 6 7 8 9 10"
-lst = string.split(" ")
-ints = [int(i) for i in lst]
-print("ints: ", ints)
-
-# Could you do that all on one line?
-# sure...
-print([int(i) for i in "1 2 3 4 5 6 7 8 9 10".split(" ")])
